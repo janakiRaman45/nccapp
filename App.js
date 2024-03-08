9e709549cbd0bb2a215a0679e4462dff84393245
janakiRaman45 <janakiraman200466@gmail.com>
2024-03-08 10:59:24 +0530

    update3

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Register from './screen/Register';
 import GridCalendar from './screen/calendar';
 import 'react-native-gesture-handler';
 import HomeScreen from './screen/Home';
-//import PDFViewer from './screen/pdf';
+
 
 const Stack = createStackNavigator();
 
@@ -14,10 +14,10 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-        <Stack.Screen name="Register" component={Register} options={{headershow: false}} />
+        <Stack.Screen name="Login" component={Login} options={{headerhown: false}} />
+        <Stack.Screen name="Register" component={Register} options={{headerShow: false}} />
         <Stack.Screen name="GridCalendar" component={GridCalendar} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headershow: false}} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShow: false}} />
         
       </Stack.Navigator>
     </NavigationContainer>
